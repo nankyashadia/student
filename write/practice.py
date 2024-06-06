d5d60fbf279841f285b65f2ef2dcb7592d81dc55
nankyashadia <shadianankya979@gmail.com>
2024-06-06 10:19:00 +0300

    Added something

--- write/practice.py
+++ write/practice.py
@@ -2,3 +2,10 @@ name ="Nankya Shadia"
 #print (name)
 course ="Bachelor of Science in Software Engineering"
 print("My name is",name, "and my course is" ,course)
+# A User inputting the details on running
+name=input("Whats your name?")
+hobbies=input("What are your hobbies?")
+address=input("Where do you come from?")
+print (name+ hobbies +address)
+#print (hobbies)
+#print(address)
